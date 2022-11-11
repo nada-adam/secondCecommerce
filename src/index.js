5aa2bf7134051475990f0659b1f1810d68af6fae
nada-adam <mh66nn7@gmail.com>
2022-11-11 17:49:35 +0300

    example

--- src/index.js
+++ src/index.js
@@ -20,4 +20,57 @@ $(function () {
     
 
   });
+  // عندما تتغير كمية المنتج
+  $('[data-product-quantity]').on( "change",function() {
+      
+    // اجلب الكمية الجديدة
+    var newQuantity = $(this).val();
+
+    // ابحث عن السّطر الّذي يحتوي معلومات هذا المُنتج
+    var $parent = $(this).parents('[data-product-info]');
+
+    // اجلب سعر القطعة الواحدة من معلومات المنتج
+    var pricePerUnit = $parent.attr('data-product-price');
+
+    // السعر الإجمالي للمنتج هو سعر القطعة مضروبًا بعددها
+    var totalPriceForProduct = newQuantity * pricePerUnit;
+
+    // عين السعر الجديد ضمن خليّة السّعر الإجمالي للمنتج في هذا السطر
+    $parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+
+    // حدث السعر الإجمالي لكل المُنتجات
+    calculateTotalPrice();
+});
+
+$('[data-remove-from-cart]').on( "click",function() { 
+  $(this).parents('[data-product-info]').remove();
+
+  // أعد حساب السعر الإجمالي بعد حذف أحد المُنتجات
+  calculateTotalPrice();
+});
+
+  function calculateTotalPrice() {
+      
+    // أنشئ متغيّرًا جديدًا لحفظ السعر الإجمالي
+    var totalPriceForAllProducts = 0;
+
+    // لكل سطر يمثل معلومات المُنتج في الصّفحة
+    $('[data-product-info]').each(function() {
+
+        // اجلب سعر القطعة الواحدة من الخاصّية الموافقة
+        var pricePerUnit = $(this).attr('data-product-price');
+
+        // اجلب كمية المنتج من حقل اختيار الكمية
+        var quantity = $(this).find('[data-product-quantity]').val();
+
+        var totalPriceForProduct = pricePerUnit * quantity;
+
+        // أضف السعر الإجمالي لهذا المنتج إلى السعر الإجمالي لكل المُنتجات، واحفظ القيمة في المتغير نفسه
+        totalPriceForAllProducts = totalPriceForAllProducts + (totalPriceForProduct);
+    });
+
+      // حدث السعر الإجمالي لكل المُنتجات في الصفحة
+    $('#total-price-for-all-products').text(totalPriceForAllProducts + '$');
+  }
+
 });
